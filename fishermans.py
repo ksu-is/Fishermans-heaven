5e9233072bc438ad3884505748c725f2c05672a1
Peyton <pwhitene@students.kennesaw.edu>
2022-11-16 10:55:10 -0500

    Update fishermans.py

--- fishermans.py
+++ fishermans.py
@@ -20,23 +20,26 @@ while fishing == True:
     if answer == "yes":
         fishing = False
         er = float(e / (a + b + c + d))
-        print("--------------------------------")
-        print("       You caught a bass!       ")
-        print("You caught:", str(a), "cod, ", str(b), "salmon, ", str(c), "shark, ", str(d), "wildfish. \nEfficiency Rate: ", str(er), ".")
+        print("You caught a bass!")
+        print("You caught:", str(a), "catfish, ", str(b), "bass, ", str(c), "shark, ", str(d), "redfish. \nEfficiency Rate: ", str(er), ".")
     else:
         t = random.randrange(1, 7)
         if t == 1:
             a += 1
-            print("You caught a cod!")
+            print("You caught a catfish!")
+            print("This fish is located in freshwater")
         elif t == 2:
             b += 1
-            print("You caught a salmon!")
+            print("You caught a bass!")
+            print("This fish is located in freshwater")
         elif t == 3:
             c += 1
             print("You caught a shark!")
+            print("This fish is located in saltwater")
         elif t == 4:
             d += 1
-            print("You caught a wildfish!")
+            print("You caught a redfish!")
+            print("This fish is located in saltwater")
         elif t >= 5:
             e += 1
             print("You caught nothing!")
